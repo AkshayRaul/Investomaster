5e650fd84a68641bc0086186fa539ddc642dcff0
shwetha <shwethakraman57@gmail.com>
2017-01-06 21:40:50 +0530

    Toolbar+refresh button

--- app/src/main/java/com/example/akshay/codeit/MainActivity.java
+++ app/src/main/java/com/example/akshay/codeit/MainActivity.java
@@ -1,13 +1,25 @@
 package com.example.akshay.codeit;
-
+import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+
+
+
     }
+
 }
